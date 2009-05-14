6e8f07840d9fbf2bd658d771d48db5a7e0b037fc
egonw <egonw@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-05-14 11:04:07 +0000

    refactored out a plugin net.bioclipse.managers for manager stuff
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@10373 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.rdf/src/net/bioclipse/rdf/business/IRDFManager.java
+++ plugins/net.bioclipse.rdf/src/net/bioclipse/rdf/business/IRDFManager.java
@@ -17,7 +17,7 @@ import net.bioclipse.core.PublishedClass;
 import net.bioclipse.core.PublishedMethod;
 import net.bioclipse.core.Recorded;
 import net.bioclipse.core.business.BioclipseException;
-import net.bioclipse.core.business.IBioclipseManager;
+import net.bioclipse.managers.business.IBioclipseManager;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.CoreException;
