753713bf0cfcf654f1b34b6f9de48322077e30b0
egonw <egonw@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-05-29 07:24:33 +0000

    Towards unit testing...
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@10829 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.structuredb.rdf/src/net/bioclipse/structuredb/rdf/business/IStructureRDFManager.java
+++ plugins/net.bioclipse.structuredb.rdf/src/net/bioclipse/structuredb/rdf/business/IStructureRDFManager.java
@@ -10,9 +10,11 @@
  ******************************************************************************/
 package net.bioclipse.structuredb.rdf.business;
 
+import net.bioclipse.core.TestClasses;
 import net.bioclipse.core.business.IBioclipseManager;
 import net.bioclipse.structuredb.business.IStructuredbManager;
 
+@TestClasses(value="net.bioclipse.structuredb.rdf.business.tests.StructureRDFManagerTest")
 public interface IStructureRDFManager extends IStructuredbManager, IBioclipseManager {
 
 }
