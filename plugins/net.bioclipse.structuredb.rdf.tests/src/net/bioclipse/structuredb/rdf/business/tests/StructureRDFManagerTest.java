05e7cce5fcfa590a39bd117265ecc4c7d607eeae
egonw <egonw@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-05-29 07:25:14 +0000

    Added test data and setUp method for manager tests
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@10830 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.structuredb.rdf.tests/src/net/bioclipse/structuredb/rdf/business/tests/StructureRDFManagerTest.java
+++ plugins/net.bioclipse.structuredb.rdf.tests/src/net/bioclipse/structuredb/rdf/business/tests/StructureRDFManagerTest.java
@@ -1,5 +1,6 @@
 /*******************************************************************************
  * Copyright (c) 2009  Egon Willighagen <egonw@users.sf.net>
+ *               2008  Jonathan Alverson
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -10,15 +11,38 @@
  ******************************************************************************/
 package net.bioclipse.structuredb.rdf.business.tests;
 
-import org.junit.Assert;
-import org.junit.Test;
+import java.util.List;
 
+import net.bioclipse.cdk.business.CDKManager;
+import net.bioclipse.cdk.business.ICDKManager;
+import net.bioclipse.cdk.domain.ICDKMolecule;
+import net.bioclipse.core.MockIFile;
 import net.bioclipse.core.business.IBioclipseManager;
 import net.bioclipse.core.tests.AbstractManagerTest;
+import net.bioclipse.structuredb.business.IStructuredbManager;
+import net.bioclipse.structuredb.domain.DBMolecule;
 import net.bioclipse.structuredb.rdf.business.StructureRDFManager;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 public class StructureRDFManagerTest extends AbstractManagerTest {
 
+    private static IStructuredbManager manager;
+    private final static String database1 = "database1";
+    private final static String database2 = "database2";
+
+    private ICDKManager cdk = new CDKManager();
+
+    @BeforeClass
+    public static void setup() throws Exception {
+        manager = new StructureRDFManager();
+        Assert.assertNotNull(manager);
+        manager.createDatabase(database1);
+        manager.createDatabase(database2);
+    }
+    
     public IBioclipseManager getManager() {
         return new StructureRDFManager();
     }
@@ -41,6 +65,49 @@ public class StructureRDFManagerTest extends AbstractManagerTest {
 
     @Test public void testAllLabels() {
         Assert.fail("Not implemented yet.");
+    }
+    
+    public void testCreatingAndRetrievingStructures() throws Exception {
+        ICDKManager cdk = new CDKManager();
+
+        ICDKMolecule mol1 = cdk.loadMolecule( 
+            new MockIFile( StructureRDFManagerTest.class
+                                   .getClassLoader()
+                                   .getResourceAsStream(
+                                       "testData/0037.cml") ) );
+        Assert.assertNotNull(mol1);
+
+        DBMolecule structure1 = manager
+                                .createMolecule( database1,
+                                                 "0037",
+                                                 mol1 );
+        Assert.assertNotNull(structure1);
+
+        DBMolecule structure2 
+            = manager.createMolecule(
+                database1,
+                "0106",
+                cdk.loadMolecule( 
+                    new MockIFile( StructureRDFManagerTest.class
+                                   .getClassLoader()
+                                   .getResourceAsStream(
+                                       "testData/0106.cml") ) ) );
+
+        Assert.assertNotNull(structure2);
+
+        Assert.assertTrue( manager
+                    .allMoleculesByName( database1,
+                                         structure1.getName() )
+                    .contains(structure1) );
+        Assert.assertTrue( manager
+                    .allMoleculesByName( database1,
+                                         structure2.getName() )
+                    .contains(structure2) );
+
+        List<DBMolecule> dBMolecules = manager.allMolecules(database1);
+
+        Assert.assertTrue( dBMolecules.contains(structure1) );
+        Assert.assertTrue( dBMolecules.contains(structure2) );
     }
 
     @Test public void testCreatingChoiceAnnotation() {
