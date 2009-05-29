4ecbc167ab6b616e851af2110b9715cfe4780741
egonw <egonw@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-05-29 07:24:33 +0000

    Towards unit testing...
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@10829 fcb5ba71-d80d-0410-8237-ba3920747fcc

---
+++ plugins/net.bioclipse.structuredb.rdf.tests/src/net/bioclipse/structuredb/rdf/business/tests/StructureRDFManagerTest.java
@@ -0,0 +1,94 @@
+/*******************************************************************************
+ * Copyright (c) 2009  Egon Willighagen <egonw@users.sf.net>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contact: Bioclipse Project <http://www.bioclipse.net>
+ ******************************************************************************/
+package net.bioclipse.structuredb.rdf.business.tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import net.bioclipse.core.business.IBioclipseManager;
+import net.bioclipse.core.tests.AbstractManagerTest;
+import net.bioclipse.structuredb.rdf.business.StructureRDFManager;
+
+public class StructureRDFManagerTest extends AbstractManagerTest {
+
+    public IBioclipseManager getManager() {
+        return new StructureRDFManager();
+    }
+
+    @Test public void testEditDBMolecule() {
+        Assert.fail("Not implemented yet.");
+    }
+
+    @Test public void testEditTextAnnotation() {
+        Assert.fail("Not implemented yet.");
+    }
+
+    @Test public void testAddMoleculesFromSDF() {
+        Assert.fail("Not implemented yet.");
+    }
+
+    @Test public void testCreatingAndRetrievingAnnotations() {
+        Assert.fail("Not implemented yet.");
+    }
+
+    @Test public void testAllLabels() {
+        Assert.fail("Not implemented yet.");
+    }
+
+    @Test public void testCreatingChoiceAnnotation() {
+        Assert.fail("Not implemented yet.");
+    }
+
+    @Test public void testCreatingRealNumberAnnotation() {
+        Assert.fail("Not implemented yet.");
+    }
+
+    @Test public void testCreatingTextAnnotation() {
+        Assert.fail("Not implemented yet.");
+    }
+
+    @Test public void testDeleteAnnotation() {
+        Assert.fail("Not implemented yet.");
+    }
+
+    @Test public void testRemovingDatabaseInstance() {
+        Assert.fail("Not implemented yet.");
+    }
+
+    @Test public void testDeleteStructure() {
+        Assert.fail("Not implemented yet.");
+    }
+
+    @Test public void testDeletingAnnotationWithMolecules() {
+        Assert.fail("Not implemented yet.");
+    }
+
+    @Test public void testListSMARTSQueryResults() {
+        Assert.fail("Not implemented yet.");
+    }
+
+    @Test public void testSmartsQueryIterator() {
+        Assert.fail("Not implemented yet.");
+    }
+
+    @Test public void testListSubstructureSearchResults() {
+        Assert.fail("Not implemented yet.");
+    }
+
+    @Test public void testSubstructureSearch() {
+        Assert.fail("Not implemented yet.");
+    }
+
+    @Test public void testCreatingAndRetrievingUsers() {
+        Assert.fail("Not implemented yet.");
+    }
+
+}
