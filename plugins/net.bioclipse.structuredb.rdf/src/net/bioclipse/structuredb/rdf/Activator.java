4436c219ddc230db221bb6d70592c9fac725a5c8
egonw <egonw@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-05-29 07:21:36 +0000

    Renamed things properly
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@10824 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.structuredb.rdf/src/net/bioclipse/structuredb/rdf/Activator.java
+++ plugins/net.bioclipse.structuredb.rdf/src/net/bioclipse/structuredb/rdf/Activator.java
@@ -1,17 +1,17 @@
 /*******************************************************************************
  * Copyright (c) 2008-2009  Egon Willighagen <egonw@users.sf.net>
- * 
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * www.eclipse.org�epl-v10.html <http://www.eclipse.org/legal/epl-v10.html>
- * 
+ *
  * Contact: http://www.bioclipse.net/
  ******************************************************************************/
-package net.bioclipse.rdf;
+package net.bioclipse.structuredb.rdf;
 
 import net.bioclipse.core.util.LogUtils;
-import net.bioclipse.rdf.business.IRDFManager;
+import net.bioclipse.structuredb.business.IStructuredbManager;
 
 import org.apache.log4j.Logger;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
@@ -20,21 +20,21 @@ import org.osgi.util.tracker.ServiceTracker;
 
 public class Activator extends AbstractUIPlugin {
 
-    public static final String PLUGIN_ID = "net.bioclipse.rdf";
+    public static final String PLUGIN_ID = "net.bioclipse.structuredb.rdf";
 
     private static final Logger logger = Logger.getLogger(Activator.class);
 
     private static Activator plugin;
     private ServiceTracker finderTracker;
-    
+
     public Activator() {}
 
     public void start(BundleContext context) throws Exception {
         super.start(context);
         plugin = this;
         finderTracker = new ServiceTracker(
-            context, 
-            IRDFManager.class.getName(), 
+            context,
+            IStructuredbManager.class.getName(),
             null
         );
         finderTracker.open();
@@ -49,10 +49,10 @@ public class Activator extends AbstractUIPlugin {
         return plugin;
     }
 
-    public IRDFManager getManager() {
-        IRDFManager manager = null;
+    public IStructuredbManager getManager() {
+        IStructuredbManager manager = null;
         try {
-            manager = (IRDFManager)finderTracker.waitForService(1000*10);
+            manager = (IStructuredbManager)finderTracker.waitForService(1000*10);
         } catch (InterruptedException exception) {
             LogUtils.debugTrace(logger, exception);
             throw new IllegalStateException("Could not get the RDF manager: " +
---
+++ plugins/net.bioclipse.structuredb.rdf/src/net/bioclipse/structuredb/rdf/business/StructureRDFManager.java
@@ -0,0 +1,228 @@
+/*******************************************************************************
+ * Copyright (c) 2009  Egon Willighagen <egonw@users.sf.net>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contact: http://www.bioclipse.net/
+ ******************************************************************************/
+package net.bioclipse.structuredb.rdf.business;
+
+import java.util.Iterator;
+import java.util.List;
+
+import net.bioclipse.cdk.domain.ICDKMolecule;
+import net.bioclipse.core.business.BioclipseException;
+import net.bioclipse.core.domain.IMolecule;
+import net.bioclipse.structuredb.business.IDatabaseListener;
+import net.bioclipse.structuredb.business.IStructuredbManager;
+import net.bioclipse.structuredb.domain.Annotation;
+import net.bioclipse.structuredb.domain.ChoiceAnnotation;
+import net.bioclipse.structuredb.domain.DBMolecule;
+import net.bioclipse.structuredb.domain.RealNumberAnnotation;
+import net.bioclipse.structuredb.domain.TextAnnotation;
+import net.bioclipse.structuredb.domain.User;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.IProgressMonitor;
+
+public class StructureRDFManager implements IStructuredbManager {
+
+    public String getNamespace() {
+        return "structrdf";
+    }
+
+    public void addListener(IDatabaseListener listener) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void addMoleculesFromSDF(String databaseName, String filePath)
+            throws BioclipseException {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void addMoleculesFromSDF(String databaseName, IFile file,
+            IProgressMonitor monitor) throws BioclipseException {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void addMoleculesFromSDF(String databaseName, IFile file)
+            throws BioclipseException {
+        // TODO Auto-generated method stub
+
+    }
+
+    public List<Annotation> allAnnotations(String databaseName) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public List<String> allDatabaseNames() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public List<TextAnnotation> allLabels(String databaseName) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public List<DBMolecule> allMolecules(String databaseName) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public List<DBMolecule> allMoleculesByName(String databaseName,
+            String structureName) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public List<User> allUsers(String databaseName) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public ChoiceAnnotation createChoiceAnnotation(String databaseName,
+            String propertyName, String value) throws IllegalArgumentException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public void createDatabase(String databaseName)
+            throws IllegalArgumentException {
+        // TODO Auto-generated method stub
+
+    }
+
+    public DBMolecule createMolecule(String databaseName, String moleculeName,
+            ICDKMolecule cdkMolecule) throws BioclipseException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public RealNumberAnnotation createRealNumberAnnotation(String databaseName,
+            String propertyName, double value) throws IllegalArgumentException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public TextAnnotation createTextAnnotation(String databaseName,
+            String propertyName, String value) throws IllegalArgumentException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public User createUser(String databaseName, String username,
+            String password, boolean sudoer) throws IllegalArgumentException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public void deleteAnnotation(String database, Annotation annotation) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void deleteDatabase(String databaseName) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void deleteStructure(String database, DBMolecule molecule) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void deleteWithMolecules(String databaseName, Annotation annotation) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void deleteWithMolecules(String name, Annotation annotation,
+            IProgressMonitor monitor) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public DBMolecule moleculeAtIndexInLabel(String databaseName, int index,
+            TextAnnotation annotation) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public int numberOfMoleculesInLabel(String databaseName,
+            TextAnnotation annotation) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    public void removeListener(IDatabaseListener listener) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void save(String database, DBMolecule molecule) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void save(String database, Annotation annotation) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public List<DBMolecule> smartsQuery(String database, String smarts) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public List<DBMolecule> smartsQuery(String database, String smarts,
+            IProgressMonitor monitor) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public Iterator<DBMolecule> smartsQueryIterator(String database,
+            String smarts) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public Iterator<DBMolecule> smartsQueryIterator(String database,
+            String smarts, IProgressMonitor monitor) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public List<DBMolecule> subStructureSearch(String databaseName,
+            IMolecule molecule) throws BioclipseException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public List<DBMolecule> subStructureSearch(String databaseName,
+            IMolecule molecule, IProgressMonitor monitor)
+            throws BioclipseException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public Iterator<DBMolecule> subStructureSearchIterator(String databaseName,
+            IMolecule molecule) throws BioclipseException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public User userByName(String databaseName, String username) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+
+}
