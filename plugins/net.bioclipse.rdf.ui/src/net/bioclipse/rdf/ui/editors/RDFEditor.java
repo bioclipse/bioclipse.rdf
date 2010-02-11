a07e0446fb8c025289eee09cc2d02e508d27d94e
Egon Willighagen <egon.willighagen@gmail.com>
2010-02-11 08:58:49 +0100

    Enabled translation of RDF triples into cheminformatics interpretable IBioObjects. Unfinished:
    * introduces an unwanted dependency: to be replaced by an extension point
    * reacts on click; needs to open an editor upon double click

--- plugins/net.bioclipse.rdf.ui/src/net/bioclipse/rdf/ui/editors/RDFEditor.java
+++ plugins/net.bioclipse.rdf.ui/src/net/bioclipse/rdf/ui/editors/RDFEditor.java
@@ -13,6 +13,9 @@ package net.bioclipse.rdf.ui.editors;
 import java.io.ByteArrayInputStream;
 import java.util.List;
 
+import net.bioclipse.cdk.business.CDKManager;
+import net.bioclipse.cdk.domain.ICDKMolecule;
+import net.bioclipse.core.business.BioclipseException;
 import net.bioclipse.rdf.business.IRDFStore;
 import net.bioclipse.rdf.business.JenaModel;
 import net.bioclipse.rdf.business.RDFManager;
@@ -21,9 +24,15 @@ import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResourceChangeEvent;
 import org.eclipse.core.resources.IResourceChangeListener;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.ListenerList;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.ISelectionProvider;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
+import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorSite;
 import org.eclipse.ui.ISelectionListener;
@@ -34,16 +43,60 @@ import org.eclipse.zest.core.viewers.GraphViewer;
 import org.eclipse.zest.layouts.LayoutStyles;
 import org.eclipse.zest.layouts.algorithms.SpringLayoutAlgorithm;
 
+import com.hp.hpl.jena.rdf.model.Literal;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.NodeIterator;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.StmtIterator;
+import com.hp.hpl.jena.vocabulary.RDF;
+
 public class RDFEditor
 extends EditorPart implements ISelectionListener ,
-	IResourceChangeListener {
+	IResourceChangeListener, ISelectionProvider
+	{
 
-    private GraphViewer viewer;
+    private final class ResourceSelectedListener implements
+			ISelectionChangedListener {
+		@Override
+		public void selectionChanged(SelectionChangedEvent event) {
+			ISelection selection = event.getSelection();
+			if (selection instanceof StructuredSelection) {
+				Object firstElem = ((StructuredSelection)selection).getFirstElement();
+				if (firstElem instanceof Resource) {
+					Resource res = (Resource)firstElem;
+					Model model = ((JenaModel)store).getModel();
+					StmtIterator iter = model.listStatements(
+						res, RDF.type, model.createResource("http://www.bioclipse.net/structuredb/#Molecule")
+					);
+					if (iter.hasNext()) {
+						// it's a molecule :)
+						NodeIterator smileses = model.listObjectsOfProperty(
+							res, model.createProperty("http://rdf.openmolecules.net/?smiles")
+						);
+						if (smileses.hasNext()) {
+							Literal smiles = (Literal)smileses.next();
+							// just take the first
+							try {
+								ICDKMolecule mol = cdk.fromSMILES(smiles.getString());
+								setSelection(new StructuredSelection(mol));
+							} catch (BioclipseException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	private GraphViewer viewer;
     private RDFContentProvider contentProvider;
 
     private IRDFStore store = null; 
     
     RDFManager rdf = new RDFManager();
+    CDKManager cdk = new CDKManager();
 
 	public RDFEditor() {
 		super();
@@ -83,14 +136,18 @@ extends EditorPart implements ISelectionListener ,
         viewer.setContentProvider(contentProvider);
         viewer.setLabelProvider(new RDFLabelProvider());
         viewer.setLayoutAlgorithm(new SpringLayoutAlgorithm(LayoutStyles.NO_LAYOUT_NODE_RESIZING), true);
+        viewer.addSelectionChangedListener(
+        	new ResourceSelectedListener()
+        );
         updateModel();
         getEditorSite().getPage().addSelectionListener(this);
+        getSite().setSelectionProvider(this);
 	}
 
 	private void updateModel() {
 		if (store == null) {
 			try {
-				IRDFStore store = rdf.createInMemoryStore();
+				this.store = rdf.createInMemoryStore();
 				readEditorInputIntoStore(store);
 				contentProvider.setModel(((JenaModel)store).getModel());
 				List<List<String>> results = rdf.sparql(store,
@@ -152,6 +209,41 @@ extends EditorPart implements ISelectionListener ,
 	@Override
 	public void resourceChanged(IResourceChangeEvent event) {
 		updateModel();
+	}
+
+	private ListenerList listeners = new ListenerList();
+
+	@Override
+	public void addSelectionChangedListener(ISelectionChangedListener listener) {
+		listeners.add(listener);
+	}
+
+	private ISelection selection = null;
+	
+	@Override
+	public ISelection getSelection() {
+		return this.selection;
+	}
+
+	@Override
+	public void removeSelectionChangedListener(
+			ISelectionChangedListener listener) {
+		listeners.remove(listener);
+	}
+
+	@Override
+	public void setSelection(ISelection selection) {
+		this.selection = selection;
+		final SelectionChangedEvent event = new SelectionChangedEvent(this, selection);
+
+		for (Object listenerO : listeners.getListeners()) {
+			final ISelectionChangedListener listener = (ISelectionChangedListener)listenerO;
+            Display.getDefault().asyncExec( new Runnable() {
+                public void run() {
+                    listener.selectionChanged(event);
+                }
+            });
+		}
 	}
 
 }
