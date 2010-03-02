f19d9b87947d64b5789475ce8773f99230fd8520
Egon Willighagen <egon.willighagen@gmail.com>
2010-03-02 15:38:06 +0100

    Added IRDFResource interface, for which we want to show the fields in the properties view

---
+++ plugins/net.bioclipse.rdf/src/net/bioclipse/rdf/model/IRDFResource.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2010  Egon Willighagen <egonw.willighagen@gmail.com>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contact: http://www.bioclipse.net/
+ *******************************************************************************/
+package net.bioclipse.rdf.model;
+
+/**
+ * Generalization of an RDF Resource.
+ */
+public interface IRDFResource {
+
+	/**
+	 * Returns the rdfs:label value, or an empty String if not defined.
+	 *
+	 * @return A non-null, possibly zero-length String.
+	 */
+	public String getRDFSLabel();
+
+	/**
+	 * Returns the rdf:type value, or an empty String if not defined.
+	 *
+	 * @return A non-null, possibly zero-length String.
+	 */
+	public String getRDFType();
+    
+}
