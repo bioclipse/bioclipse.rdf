9bf8617739cad01fb6d6b06029bf5bc136575c7a
jonalv <jonathan.alvarsson@gmail.com>
2010-03-03 14:24:39 +0100

    Added PropertySources to RDF domain objects

--- plugins/net.bioclipse.rdf/src/net/bioclipse/rdf/model/JenaRDFLiteral.java
+++ plugins/net.bioclipse.rdf/src/net/bioclipse/rdf/model/JenaRDFLiteral.java
@@ -10,6 +10,14 @@
  *******************************************************************************/
 package net.bioclipse.rdf.model;
 
+import java.util.ArrayList;
+
+import org.eclipse.ui.views.properties.IPropertyDescriptor;
+import org.eclipse.ui.views.properties.IPropertySource;
+import org.eclipse.ui.views.properties.PropertyDescriptor;
+import org.eclipse.ui.views.properties.TextPropertyDescriptor;
+
+
 /**
  * Jena-based implementation of {@link IRDFResource}.
  */
@@ -41,13 +49,77 @@ public class JenaRDFLiteral implements IRDFLiteral {
 		return "";
 	}
 
-	public Object getAdapter(Class adapter) {
-		// TODO Auto-generated method stub
+	@SuppressWarnings("unchecked")
+    public Object getAdapter(Class adapter) {
+		if ( adapter.isAssignableFrom( IPropertySource.class ) ) {
+		    return new PropertySource( this );
+		}
 		return null;
 	}
 
 	public String getValue() {
 		return this.value;
 	}
-    
+
+
+	/**
+	 * The PropertySource is used by the properties view.
+	 *
+	 * @author jonalv
+	 */
+	public static class PropertySource implements IPropertySource {
+
+	    private JenaRDFLiteral literal;
+	    private IPropertyDescriptor[] propertyDescriptors;
+
+	    public static final String PROPERTY_LANGUAGE = "Language";
+	    public static final String PROPERTY_DATATYPE = "Data type";
+
+	    public PropertySource(JenaRDFLiteral literal) {
+	        this.literal = literal;
+	    }
+
+        public Object getEditableValue() {
+            return null;
+        }
+
+        public IPropertyDescriptor[] getPropertyDescriptors() {
+            if ( propertyDescriptors == null ) {
+                ArrayList<IPropertyDescriptor> properties
+                    = new ArrayList<IPropertyDescriptor>();
+
+                for ( String p : new String[] { PROPERTY_LANGUAGE,
+                                                PROPERTY_DATATYPE } ) {
+                    PropertyDescriptor descriptor
+                        = new TextPropertyDescriptor(p, p);
+                    descriptor.setCategory("RDF");
+                    properties.add(descriptor);
+                }
+
+                propertyDescriptors = properties.toArray(
+                                          new IPropertyDescriptor[2] );
+            }
+            return propertyDescriptors;
+        }
+
+        public Object getPropertyValue( Object id ) {
+            if (PROPERTY_LANGUAGE.equals( id ) ) {
+                return literal.language;
+            }
+            if ( PROPERTY_DATATYPE.equals( id ) ) {
+                return literal.datatype;
+            }
+            return null;
+        }
+
+        public boolean isPropertySet( Object id ) {
+            return false;
+        }
+
+        public void resetPropertyValue( Object id ) {
+        }
+
+        public void setPropertyValue( Object id, Object value ) {
+        }
+	}
 }
--- plugins/net.bioclipse.rdf/src/net/bioclipse/rdf/model/JenaRDFResource.java
+++ plugins/net.bioclipse.rdf/src/net/bioclipse/rdf/model/JenaRDFResource.java
@@ -10,6 +10,13 @@
  *******************************************************************************/
 package net.bioclipse.rdf.model;
 
+import java.util.ArrayList;
+
+import org.eclipse.ui.views.properties.IPropertyDescriptor;
+import org.eclipse.ui.views.properties.IPropertySource;
+import org.eclipse.ui.views.properties.PropertyDescriptor;
+import org.eclipse.ui.views.properties.TextPropertyDescriptor;
+
 import com.hp.hpl.jena.rdf.model.Literal;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.RDFNode;
@@ -30,7 +37,7 @@ public class JenaRDFResource implements IRDFResource {
 		this.model = model;
 		this.resource = resource;
 	}
-	
+
 	/**
 	 * Returns the rdfs:label value, or an empty String if not defined.
 	 *
@@ -63,9 +70,72 @@ public class JenaRDFResource implements IRDFResource {
 		return "";
 	}
 
-	public Object getAdapter(Class adapter) {
-		// TODO Auto-generated method stub
-		return null;
+	@SuppressWarnings("unchecked")
+    public Object getAdapter(Class adapter) {
+	    if ( adapter.isAssignableFrom( IPropertySource.class ) ) {
+	        return new PropertySource( this );
+	    }
+	    return null;
 	}
-    
+
+	 /**
+     * The PropertySource is used by the properties view.
+     *
+     * @author jonalv
+     */
+    public static class PropertySource implements IPropertySource {
+
+        private JenaRDFResource resource;
+        private IPropertyDescriptor[] propertyDescriptors;
+
+        public static final String PROPERTY_TYPE  = "Type";
+        public static final String PROPERTY_LABEL = "Label";
+
+        public PropertySource(JenaRDFResource resource) {
+            this.resource = resource;
+        }
+
+        public Object getEditableValue() {
+            return null;
+        }
+
+        public IPropertyDescriptor[] getPropertyDescriptors() {
+            if ( propertyDescriptors == null ) {
+                ArrayList<IPropertyDescriptor> properties
+                    = new ArrayList<IPropertyDescriptor>();
+
+                for ( String p : new String[] { PROPERTY_TYPE,
+                                                PROPERTY_LABEL } ) {
+                    PropertyDescriptor descriptor
+                        = new TextPropertyDescriptor(p, p);
+                    descriptor.setCategory("RDF");
+                    properties.add(descriptor);
+                }
+
+                propertyDescriptors = properties.toArray(
+                                          new IPropertyDescriptor[2] );
+            }
+            return propertyDescriptors;
+        }
+
+        public Object getPropertyValue( Object id ) {
+            if (PROPERTY_TYPE.equals( id ) ) {
+                return resource.getRDFType();
+            }
+            if ( PROPERTY_LABEL.equals( id ) ) {
+                return resource.getRDFSLabel();
+            }
+            return null;
+        }
+
+        public boolean isPropertySet( Object id ) {
+            return false;
+        }
+
+        public void resetPropertyValue( Object id ) {
+        }
+
+        public void setPropertyValue( Object id, Object value ) {
+        }
+    }
 }
