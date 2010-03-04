8f289dd5e5c06ca25940c622bda45b8cf8023725
Egon Willighagen <egon.willighagen@gmail.com>
2010-03-04 14:57:09 +0100

    Merged copyright statements: one is clearer; improved header format to use less characters and still not be JavaDoc

--- plugins/net.bioclipse.rdf.ui/src/net/bioclipse/rdf/ui/editors/RDFEditor.java
+++ plugins/net.bioclipse.rdf.ui/src/net/bioclipse/rdf/ui/editors/RDFEditor.java
@@ -1,25 +1,11 @@
-/*******************************************************************************
- * Copyright (c) 2009-2010  Egon Willighagen <egonw@users.sf.net>
-
+/* Copyright (c) 2009-2010  Egon Willighagen <egonw@users.sf.net>
+ *                    2010  Jonathan Alvarsson <jonalv@users.sourceforge.net>
  *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * www.eclipse.orgâ€”epl-v10.html <http://www.eclipse.org/legal/epl-v10.html>
- *
- * Contact: http://www.bioclipse.net/
- ******************************************************************************/
-/*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
- |  Copyright (c) 2010  Jonathan Alvarsson <jonalv@users.sourceforge.net>    |
- *                                                                           *
- |  All rights reserved. This program and the accompanying materials         |
- *  are made available under the terms of the Eclipse Public License v1.0    *
- |  which accompanies this distribution, and is available at                 |
- *  www.eclipse.orgÑepl-v10.html <http://www.eclipse.org/legal/epl-v10.html> *
- |                                                                           |
- *  Contact: http://www.bioclipse.net/                                       *
- |                                                                           |
- *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*/
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at                 |
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
 package net.bioclipse.rdf.ui.editors;
 
 import java.io.ByteArrayInputStream;
