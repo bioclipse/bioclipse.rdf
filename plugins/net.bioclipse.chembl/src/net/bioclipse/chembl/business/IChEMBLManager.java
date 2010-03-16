15a4328e5272a186de2c5393e9ea507265b757a3
annzi <annzi.andersson@gmail.com>
2010-03-16 10:40:49 +0100

    Added convenient methods to the chembl plug-in

--- plugins/net.bioclipse.chembl/src/net/bioclipse/chembl/business/IChEMBLManager.java
+++ plugins/net.bioclipse.chembl/src/net/bioclipse/chembl/business/IChEMBLManager.java
@@ -10,8 +10,11 @@
  ******************************************************************************/
 package net.bioclipse.chembl.business;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+
+import org.eclipse.core.resources.IFile;
 
 import net.bioclipse.core.PublishedClass;
 import net.bioclipse.core.PublishedMethod;
@@ -46,4 +49,83 @@ public interface IChEMBLManager extends IBioclipseManager {
 	)
 	public IStringMatrix getProperties(Integer targetID)
 	throws BioclipseException;
+	 @PublishedMethod(
+	            params="Integer targetID",
+	            methodSummary="Returns the sequence for a target."
+	    )
+	    public List<String> getTargetSequence(Integer targetID)
+	    throws BioclipseException;
+
+	    @PublishedMethod(
+	            params="",
+	            methodSummary="Returns targets"
+	    )
+	    public IStringMatrix getForMaris(String ion, String vgc)
+	    throws BioclipseException;
+
+	    @PublishedMethod(
+	            params="Integer targetID, String activity",
+	            methodSummary="Returns smiles, activity values, confidence score. If no specific activity write \"\" "
+	    )
+	    public IStringMatrix getQSARData2(Integer targetID, String activity)
+	    throws BioclipseException;
+
+	    
+	    @PublishedMethod(
+	            params="Integer molID",
+	            methodSummary="Returns chebi id"
+	    )
+	    public String getChebiId(Integer molID)
+	    throws BioclipseException;
+
+	    @PublishedMethod(
+	            params="String proteinSeq",
+	            methodSummary="Returns target id"
+	    )
+	    public List<String> getTargetIDWithProteinSeq(String proteinSeq)
+	    throws BioclipseException;
+	    
+	    @PublishedMethod(
+	            params="String EC ID",
+	            methodSummary="Returns target id"
+	    )
+	    public List<String> getTargetIDWithEC(String ecNumber)
+	    throws BioclipseException;
+	    
+	    @PublishedMethod(
+	            params="String key word",
+	            methodSummary="Returns target id. Search description from ChEMBL."
+	    )
+	    public IStringMatrix getTargetIDWithKeyword(String keyword)
+	    throws BioclipseException;
+	    
+	    @PublishedMethod(
+	            params="String chebiID",
+	            methodSummary="Takes chebiID or moleculeID and returns information about the compound."
+	    )
+	    public IStringMatrix getCompoundInfo(Integer chebiID)
+	    throws BioclipseException;
+	   
+	    @PublishedMethod(
+	            params="String molID",
+	            methodSummary="Takes moleculeIDs(\"1,2,3\") and returns their chebi ids."
+	    )
+	    public List<String> getChebiId(String chebiID)
+	    throws BioclipseException;
+	    
+	    @PublishedMethod(
+	            params="String molID",
+	            methodSummary="Takes moleculeIDs(\"1,2,3\") and returns their chebi ids."
+	    )
+	    public List<String> getCompoundInfo(String chebiID)
+	    throws BioclipseException;
+	    
+	    
+	    @PublishedMethod(
+	        params = "String file, StringMatrix matrix",
+	        methodSummary = ""
+	    )
+	    public void saveCSV(String file, IStringMatrix s)
+	    throws BioclipseException, IOException ;
+	
 }
