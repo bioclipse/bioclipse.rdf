f183d799b4d0ecc7af497d611f56cf6dd1a885e5
annzi <annzi.andersson@gmail.com>
2010-05-18 14:47:41 +0200

    Changed plugin_id to a correct one

--- plugins/net.bioclipse.chembl.ui/src/net/bioclipse/chembl/ui/wizard/Activator.java
+++ plugins/net.bioclipse.chembl.ui/src/net/bioclipse/chembl/ui/wizard/Activator.java
@@ -17,7 +17,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
     // The plug-in ID
-    public static final String PLUGIN_ID = "net.bioclipse.moss";
+    public static final String PLUGIN_ID = "net.bioclipse.chembl.ui";
 
     // The shared instance
     private static Activator plugin;
