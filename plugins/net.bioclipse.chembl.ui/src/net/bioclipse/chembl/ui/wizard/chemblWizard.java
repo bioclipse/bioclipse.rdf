d7cd3e5551bfe282f5f619b279b387e596af4505
annzi <annzi.andersson@gmail.com>
2010-05-18 14:51:21 +0200

    Changed class name (c->C) and removed a wizardpage

--- plugins/net.bioclipse.chembl.ui/src/net/bioclipse/chembl/ui/wizard/chemblWizard.java
+++ plugins/net.bioclipse.chembl.ui/src/net/bioclipse/chembl/ui/wizard/chemblWizard.java
@@ -1,44 +1,35 @@
 package net.bioclipse.chembl.ui.wizard;
 
 
-import net.bioclipse.chembl.ui.wizard.chemblWizardPage2;
-
+import net.bioclipse.chembl.Activator;
+import net.bioclipse.chembl.business.IChEMBLManager;
 import org.apache.log4j.Logger;
-import org.eclipse.ui.IWorkbench;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.wizard.Wizard;
 
+public class ChemblWizard extends Wizard {
 
-public class chemblWizard extends Wizard {
-
-	private static final Logger logger = Logger.getLogger(chemblWizard.class);
-
-
-	private ISelection selection;
-	//chemblWizardPageTabs firstpage;
-
-
-	private chemblWizardPage firstpage;
-	private chemblWizardPage2 secondpage;
-
-	@Override
-	public boolean performFinish() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-
-	public chemblWizard() {
+	private static final Logger logger = Logger.getLogger(ChemblWizard.class);
+	private ChemblWizardPage firstpage;
+	private IChEMBLManager chembl;
+    //ChemblData data = new ChemblData();
+  
+	public ChemblWizard() {
 		super();
 		setWindowTitle("ChEMBL wizard");
+		chembl = Activator.getDefault().getJavaChEMBLManager();
 	}
 
 	public void addPages(){
-//		firstpage = new chemblWizardPage("ChEMBL page");
-//		addPage(firstpage);
-		
-		secondpage = new chemblWizardPage2("ChEMBL MoSS page");
-		addPage(secondpage);
-	}
+		firstpage = new ChemblWizardPage("ChEMBL page");
+		addPage(firstpage);
+	}	
 
+	//Writes compounds to a MoSS supported file
+	@Override
+	public boolean performFinish() {
+	
+		return false;
+	}
+	
 }
