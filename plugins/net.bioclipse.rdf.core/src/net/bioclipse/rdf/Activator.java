e785a1daf7450777613d85923cfa09fbc3761bd3
Egon Willighagen <egon.willighagen@gmail.com>
2015-12-10 13:03:59 +0100

    Factored out IRDFStore, to not be able to use this interface without infecting with a particular Jena version (allowing a OpenRDF implementation, and possibly other Jena versions (like 3.0))

---
+++ plugins/net.bioclipse.rdf.core/src/net/bioclipse/rdf/Activator.java
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ * Copyright (c) 2008-2009  Egon Willighagen <egonw@users.sf.net>
+ * 
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * www.eclipse.org�epl-v10.html <http://www.eclipse.org/legal/epl-v10.html>
+ * 
+ * Contact: http://www.bioclipse.net/
+ ******************************************************************************/
+package net.bioclipse.rdf;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+public class Activator extends AbstractUIPlugin {
+
+    public static final String PLUGIN_ID = "net.bioclipse.rdf";
+    private static Activator plugin;
+
+    /** HTTP time out in milliseconds. */
+    public static final Integer CONNECT_TIME_OUT = 5000; 
+    public static final Integer READ_TIME_OUT = 30000; 
+
+    public Activator() {}
+
+    public void start(BundleContext context) throws Exception {
+        super.start(context);
+        plugin = this;
+    }
+
+    public void stop(BundleContext context) throws Exception {
+        plugin = null;
+        super.stop(context);
+    }
+
+    public static Activator getDefault() {
+        return plugin;
+    }
+
+}
